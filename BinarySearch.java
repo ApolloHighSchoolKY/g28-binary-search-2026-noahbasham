9b0fcd871234c36d868338ad61962c8fc4a11ab8
noahbasham <noah.basham@stu.daviess.kyschools.us>
2026-03-31 21:39:30 +0000

    commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,27 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
+		if(ray[ray.length/2]==item)
+			return ray.length/2;
 
+		else if(ray[ray.length/2]<item)
+		{
+			int[] newRay = new int[ray.length/2];
+			for(int i=0; i<newRay.length; i++)
+			{
+				newRay[i] = ray[i];
+			}
+			return binarySearch(newRay, item);
+		}
+		else
+		{
+			int[] newRay = new int[ray.length/2];
+			for(int i=ray.length/2; i<ray.length; i++)
+			{
+				newRay[i] = ray[i];
+			}
+			return binarySearch(newRay, item);
+		}
 	}
 }
